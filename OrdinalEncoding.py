f4bed2c7861d12f867d7e790818bd9bbaeb1b50a
AmickGIT <amickpvt@gmail.com>
2024-10-27 22:53:01 +0530

    ⭐ Second Commit

--- OrdinalEncoding.py
+++ OrdinalEncoding.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import numpy as np
 
 df_org = pd.read_csv('data.csv')
 df = df_org.copy()
 
-category_order = ['Never', 'Rarely', 'Most Days', 'Everyday']
+category_order = np.array(['Never', 'Rarely', 'Most Days', 'Everyday'])
 df['Breakfast_encoded'] = 'Unknown'
 
 
 for id, category in df['Breakfast'].items():
-    df.loc[id, 'Breakfast_encoded'] = category_order.index(category)
-
+    index = np.where(category_order == category)[0]
+    if index.size > 0:
+        df.loc[id, 'Breakfast_encoded'] = index[0]
+    
+df.to_csv('data_ordinal.csv')
 print(df)
